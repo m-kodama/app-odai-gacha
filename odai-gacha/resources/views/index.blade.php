cb13b20e33fbe674ea9795a397b629cd55c320cc
m-kodama <m-kodama@rhizo-me.com>
2019-10-08 15:02:56 +0900

    feat: php側でガチャの中身を送るようにできた # 20

--- odai-gacha/resources/views/index.blade.php
+++ odai-gacha/resources/views/index.blade.php
@@ -1,37 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>お題ガチャ</title>
-  <script src="js/lib/anime.min.js"></script>
-  <script src="js/util.js"></script>
-  <script src="js/class/Handle.js"></script>
-  <script src="js/class/Gacha.js"></script>
-  <script src="js/index.js"></script>
-  <link rel="stylesheet" href="css/index.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ランディングページ</title>
 </head>
 <body>
-  <div class="cp_ipselect cp_sl01">
-      <select required onchange="onSelectBoxChange(this)">
-        <option value="question" selected>シンクロナイズド・シンキング</option>
-        <option value="denpo">デンポー</option>
-      </select>
-  </div>
-  <div id="stage">
-      <div id="touch-eater"></div>
-      <img src="img/gacha_body.png" alt="" id="gacha-body" class="gacha" draggable="false">
-      <img src="img/gacha_handle.png" alt="" id="handle" class="gacha" draggable="false">
-      <img src="img/egg_normal.png" alt="" id="egg-normal" class="egg" draggable="false">
-      <img src="img/egg_silver.png" alt="" id="egg-sliver" class="egg" draggable="false">
-      <img src="img/egg_gold.png" alt="" id="egg-gold" class="egg" draggable="false">
-      <img src="img/egg_platinum.png" alt="" id="egg-platinum" class="egg" draggable="false">
-      <div id="odai">
-        <div id="odai-text"></div>
-        <div style="height: 8px;"></div>
-        <button id="close-btn">閉じる</button>
-      </div>
-  </div>
+    <h1>ToDo: Landing Page</h1>
 </body>
 </html>

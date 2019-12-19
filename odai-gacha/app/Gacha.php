fbc4e08c18c3a34075be73922fd585dc45bd4d0b
Masaaki Kodama <tc003ssr@gmail.com>
2019-12-19 23:43:37 +0900

    feat: modelにhasMany, belongsTo設定を追加

--- odai-gacha/app/Gacha.php
+++ odai-gacha/app/Gacha.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Gacha extends Model
 {
-    //
+    protected $table = 'gacha_master';
+
+    //hasMany設定
+    public function topics()
+    {
+        return $this->hasMany('App\Topic');
+    }
 }

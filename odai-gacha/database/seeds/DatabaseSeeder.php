3c6176eb45234ec9af2820eaafc2f1b816994ef9
Masaaki Kodama <tc003ssr@gmail.com>
2019-12-19 23:28:49 +0900

    feat: topicsのseedファイルを追加

--- odai-gacha/database/seeds/DatabaseSeeder.php
+++ odai-gacha/database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserTableSeeder::class);
         $this->call(GachaMasterTableSeeder::class);
         $this->call(RarityTableSeeder::class);
+        $this->call(TopicsTableSeeder::class);
     }
 }

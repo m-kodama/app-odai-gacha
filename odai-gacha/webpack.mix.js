aa1e9eb5a98e40af437b912754aa3f322e219eb9
溝口裕斗 <mizoguchihiroshito@mizo-2.local>
2020-01-14 20:54:10 +0900

    vue_install

--- odai-gacha/webpack.mix.js
+++ odai-gacha/webpack.mix.js
@@ -11,5 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+
+mix.browserSync({
+    proxy: '0.0.0.0:8000', // アプリの起動アドレス
+    open: false // ブラウザを自動で開かない
+})
+    .js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .version();

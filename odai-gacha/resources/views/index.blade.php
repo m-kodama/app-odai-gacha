15bd07a7fd7d23287003865dbb4854206f08b639
Masaaki Kodama <tc003ssr@gmail.com>
2020-01-14 23:06:43 +0900

    feat: app.js作成

--- odai-gacha/resources/views/index.blade.php
+++ odai-gacha/resources/views/index.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ランディングページ</title>
+    <title>お題ガチャ</title>
+
+    <script src="{{ mix('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="..css/app.css">
 </head>
 <body>
-    <h1>ToDo: Landing Page</h1>
+    <div id="app"></div>
 </body>
 </html>

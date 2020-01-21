fc7fa61b3debde08afee42dbc1aa27d10824e6f5
溝口裕斗 <mizoguchihiroshito@mizo-2.local>
2020-01-21 23:20:35 +0900

    feat: vuetigy, mdi導入

--- odai-gacha/resources/js/gacha/App.vue
+++ odai-gacha/resources/js/gacha/App.vue
@@ -1,18 +1,59 @@
 <template>
-  <div>hello vue {message}</div>
+   <v-app>
+    <v-content>
+             <div>
+    <v-app-bar
+      color='orange'
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-apple</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+  </div>
+    </v-content>
+    </v-app>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data: () => ({
-    message: 'こんにちはvue'
-  }),
+  // name: 'app',
+  // data: () => ({
+  //   message: 'こんにちはvue'
+  // }),
 }
 </script>
 
 <style>
-div {
-  background-color: #ff0000;
-}
+
 </style>
--- odai-gacha/resources/js/gacha/app.js
+++ odai-gacha/resources/js/gacha/app.js
@@ -1,8 +1,18 @@
 import Vue from "vue";
 import App from './App.vue';
+import '@mdi/font/css/materialdesignicons.css'
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
 
 new Vue({
   el: "#app",
+  vuetify: new Vuetify({
+    icons: {
+      iconfont: 'mdi',
+    },
+  }),
   template: "<App/>",
   components: { App: App }
 });

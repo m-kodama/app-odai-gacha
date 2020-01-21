a74f8baaff6b91ba92e6b80ceb3cf9c76bd41508
Masaaki Kodama <tc003ssr@gmail.com>
2020-01-21 23:41:01 +0900

    feat: テーマカラー設定を入れてみた

--- odai-gacha/resources/js/gacha/app.js
+++ odai-gacha/resources/js/gacha/app.js
@@ -1,18 +1,34 @@
 import Vue from "vue";
-import App from './App.vue';
-import '@mdi/font/css/materialdesignicons.css'
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
+import App from "./App.vue";
+import "@mdi/font/css/materialdesignicons.css";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify);
 
 new Vue({
-  el: "#app",
-  vuetify: new Vuetify({
-    icons: {
-      iconfont: 'mdi',
-    },
-  }),
-  template: "<App/>",
-  components: { App: App }
+    el: "#app",
+    vuetify: new Vuetify({
+        icons: {
+            iconfont: "mdi"
+        },
+        theme: {
+            themes: {
+                light: {
+                    primary: colors.purple,
+                    secondary: colors.grey.darken1,
+                    accent: colors.shades.black,
+                    error: colors.red.accent3
+                },
+                dark: {
+                    primary: colors.purple,
+                    secondary: colors.grey.darken1,
+                    accent: colors.shades.black,
+                    error: colors.red.accent3
+                }
+            }
+        }
+    }),
+    template: "<App/>",
+    components: { App: App }
 });

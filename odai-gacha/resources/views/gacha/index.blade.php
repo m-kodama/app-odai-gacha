e0cb044994d999697e2196f1b78857bc172713a5
Masaaki Kodama <tc003ssr@gmail.com>
2020-02-04 23:34:19 +0900

    feat #25 fontをCDNから取得するように設定

--- odai-gacha/resources/views/gacha/index.blade.php
+++ odai-gacha/resources/views/gacha/index.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>お題ガチャ</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
 
   <script src="{{ mix('js/gacha/app.js') }}" defer></script> 
 </head>

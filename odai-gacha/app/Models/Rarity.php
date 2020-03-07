b2327c1f5f2cfb426780eccc2911f2fc73e23810
Masaaki Kodama <tc003ssr@gmail.com>
2020-03-07 22:11:45 +0900

    feat: レア度テーブルとレア度画像テーブルのリレーションを追加

--- odai-gacha/app/Models/Rarity.php
+++ odai-gacha/app/Models/Rarity.php
@@ -11,7 +11,21 @@ class Rarity extends Model
     protected $primaryKey = 'rarity_id';
     protected $keyType = 'char';
     public $incrementing = false;
-    protected $fillable = ['rarity_id', 'rarity', 'rarity_name', 'probability', 'gacha_id', 'created_at', 'updated_at'];
+    protected $fillable = [
+        'rarity_id',
+        'rarity',
+        'rarity_name',
+        'rarity_image_id',
+        'probability',
+        'gacha_id',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function rarityImage()
+    {
+        return $this->belongsTo('App\Models\RarityImage', 'rarity_image_id');
+    }
 
     public function __construct(array $attributes = [])
     {
--- odai-gacha/database/seeds/RarityTableSeeder.php
+++ odai-gacha/database/seeds/RarityTableSeeder.php
@@ -17,6 +17,7 @@ class RarityTableSeeder extends Seeder
                 'rarity_id' => '01E02KJWM2PHQT336EEP065X4O',
                 'rarity' => 0,
                 'rarity_name' => 'ノーマル',
+                'rarity_image_id' => '01E02KJWM2PHQT336MOP065X01',
                 'probability' => 500,
                 'gacha_id' => '01E02KJWM2PHQT336EEP065X4N',
                 'created_at' => new DateTime(),
@@ -26,6 +27,7 @@ class RarityTableSeeder extends Seeder
                 'rarity_id' => '01E02KJWM2PHQT336EEP065X4P',
                 'rarity' => 1,
                 'rarity_name' => 'シルバー',
+                'rarity_image_id' => '01E02KJWM2PHQT336MOP065X02',
                 'probability' => 350,
                 'gacha_id' => '01E02KJWM2PHQT336EEP065X4N',
                 'created_at' => new DateTime(),
@@ -35,6 +37,7 @@ class RarityTableSeeder extends Seeder
                 'rarity_id' => '01E02KJWM2PHQT336EEP065X4Q',
                 'rarity' => 2,
                 'rarity_name' => 'ゴールド',
+                'rarity_image_id' => '01E02KJWM2PHQT336MOP065X03',
                 'probability' => 130,
                 'gacha_id' => '01E02KJWM2PHQT336EEP065X4N',
                 'created_at' => new DateTime(),
@@ -43,9 +46,10 @@ class RarityTableSeeder extends Seeder
             [
                 'rarity_id' => '01E02KJWM2PHQT336EEP065X4R',
                 'rarity' => 3,
-                'gacha_id' => '01E02KJWM2PHQT336EEP065X4N',
                 'rarity_name' => 'プラチナ',
+                'rarity_image_id' => '01E02KJWM2PHQT336MOP065X04',
                 'probability' => 20,
+                'gacha_id' => '01E02KJWM2PHQT336EEP065X4N',
                 'created_at' => new DateTime(),
                 'updated_at' => new DateTime()
             ]
